e735216e1b238ffc81333c919575416a0fc225f7
João Pedro Sardinha <joaopedrostcunha@protonmail.com>
2025-01-20 19:45:46 +0000

    Feito o sistema para registrar cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,16 +7,43 @@
 //Teste larissa
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+
+  char id[4], name[50];
+  // id e name = código e nome da cidade
+  float area, pib;
+  // area = área da cidade, e pib = Pib da cidade
+  int tourist_spots, population;
+  // tourist_spots = pontos turísticos da cidade, e population = número da população da cidade
+
+
+  //registrar o código da cidade:
+  printf("Digite o código da cidade: ");
+  scanf("%s", &id);
+
+  //registrar o nome da cidade:
+  printf("Digite o nome da cidade: ");
+  scanf("%s", &name);
+
+  //registrar o número da cidade:
+  printf("Digite o número da população da cidade: ");
+  scanf("%d", &population);
+
+  //registrar a área da cidade:
+  printf("Digite a área da cidade em m²: ");
+  scanf("%f", &area);
+
+  //registrar o PIB da cidade:
+  printf("Digite o PIB da cidade: ");
+  scanf("%f", &pib);
+
+  //registrar a quantidade de pontos turísticos da cidade:
+  printf("Digite o número de pontos turísticos presentes na cidade: ");
+  scanf("%d", &tourist_spots);
+
+
+  //exibir informações da carta:
+  printf("\nCarta:\n\nCódigo: %s\nNome: %s\nPopulação: %d\nÁrea: %.3fm²\nPIB: %.2f R$\nQuantidade de pontos turísticos: %d\n", 
+         id, name, population, area, pib, tourist_spots);
 
     return 0;
 }
