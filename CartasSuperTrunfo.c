aad3b339d03d9acfb4a10ac705295927cc4d28a3
JoaoPedro <joaopedrostcunha@protonmail.com>
2025-03-04 16:50:32 -0300

    concluido desafio "Aventureiro"

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,13 +7,23 @@
 // Siga os comentários para implementar cada parte do desafio.
 //Teste larissa
 
-int main() {
+float densidade_pop(int populacao, float area){
+  return populacao / area;
+}
+
+float pib_capta(float pib , int populacao){
+  return pib / populacao;
+}
+
+//n = número da carta
+char* registro_cartas(int n) {
+  static char carta[500];
 
   char estado, codigo[4], nome[50];
   // area = área da cidade, e pib = Pib da cidade
   float area, pib;
-  // tourist_spots = pontos turísticos da cidade, e population = número da população da cidade
-  int tourist_spots, population;
+  // pto_turisticos= pontos turísticos da cidade, e populacao= número da população da cidade
+  int pto_turisticos, populacao;
 
   //registrar a letra do estado da cidade:
   printf("Digite a letra do estado da cidade: ");
@@ -31,7 +41,7 @@ int main() {
 
   //registrar o número da cidade:
   printf("Digite o número da população da cidade: ");
-  scanf("%d", &population);
+  scanf("%d", &populacao);
 
   //registrar a área da cidade:
   printf("Digite a área da cidade em km²: ");
@@ -43,12 +53,31 @@ int main() {
 
   //registrar a quantidade de pontos turísticos da cidade:
   printf("Digite o número de pontos turísticos presentes na cidade: ");
-  scanf("%d", &tourist_spots);
+  scanf("%d", &pto_turisticos);
 
 
-  //exibir informações da carta:
-  printf("\nEstado: %c\nCódigo: %s\nNome: %s\nPopulação: %d\nÁrea: %.2f km²\nPIB: %.2f Bilhões de reais\nQuantidade de pontos turísticos: %d\n", 
-         estado, codigo, nome, population, area, pib, tourist_spots);
+  //exibir informações da cartas
+  sprintf(carta, "Carta %d:\nEstado: %c\nCódigo: %s\nNome: %s\nPopulação: %d habitantes\nÁrea: %.2f km²\nPIB: %.2f Bilhões de reais\nQuantidade de pontos turísticos: %d\nDensidade populacional: %.2f hab/km²\nPib per capta: %.2f reais\n",
+  n, estado, codigo, nome, populacao, area, pib, pto_turisticos, densidade_pop(populacao, area), pib_capta(pib, populacao));
 
-    return 0;
+  getchar();
+
+  return carta;
+}
+
+int main (){
+
+  char carta1[600];
+  char carta2[600];
+
+  printf("\nRegistrar carta 1: \n\n");
+  sprintf(carta1, "%s", registro_cartas(1));
+
+  printf("\nRegistrar carta 2: \n\n");
+  sprintf(carta2, "%s", registro_cartas(2));
+
+  printf("\n\n%s\n\n", carta1);
+  printf("%s", carta2);
+
+  return 0;
 }
