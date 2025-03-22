b420e3754407830d7d1e40d28ad8fa8b036ec546
Joao <joaopedrostcunha@protonmail.com>
2025-03-22 19:03:22 -0300

    Desafio Mestre

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,12 +5,16 @@
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 
-float densidade_pop(int populacao, float area){
+float densidade_pop(unsigned long int populacao, float area){
   return populacao / area;
 }
 
-float pib_capta(float pib , int populacao){
+float pib_capta(float pib , unsigned long int populacao){
   return pib / populacao;
+}
+
+float super_poder(float area, float pib, int pto_turisticos, unsigned long int populacao){
+  return area + pib + pto_turisticos + populacao + pib_capta(pib, populacao) - densidade_pop(populacao, area);
 }
 
 int main () {
@@ -18,7 +22,8 @@ int main () {
   // Registrar primeira carta
   char estado[2], codigo[4], nome[50];
   float area, pib;
-  int pto_turisticos, populacao;
+  int pto_turisticos;
+  unsigned long int populacao;
   
   printf("Registrar Carta 1:\n\n");
 
@@ -55,7 +60,8 @@ int main () {
   // Registrar segunda carta
   char estado_2, codigo_2[4], nome_2[50];
   float area_2, pib_2;
-  int pto_turisticos_2, populacao_2;
+  int pto_turisticos_2;
+  unsigned long int populacao_2;
 
   printf("\n\nRegistrar Carta 2:\n\n");
 
@@ -91,11 +97,50 @@ int main () {
   scanf("%d", &pto_turisticos_2);
 
   // Exibir informações das cartas
-  printf("\n\nCarta 1:\nEstado: %s\nCódigo: %s\nNome: %s\nPopulação: %d habitantes\nÁrea: %.2f km²\nPIB: %.2f Bilhões de reais\nQuantidade de pontos turísticos: %d\nDensidade populacional: %.2f hab/km²\nPib per capta: %.2f reais\n",
-  estado, codigo, nome, populacao, area, pib, pto_turisticos, densidade_pop(populacao, area), pib_capta(pib, populacao));
+  printf("\n\nCarta 1:\nEstado: %s\nCódigo: %s\nNome: %s\nPopulação: %d habitantes\nÁrea: %.2f km²\nPIB: %.2f Bilhões de reais\nQuantidade de pontos turísticos: %d\nDensidade populacional: %.2f hab/km²\nPib per capta: %.2f reais\nSuper poder: %.2f\n",
+  estado, codigo, nome, populacao, area, pib, pto_turisticos, densidade_pop(populacao, area), pib_capta(pib, populacao), super_poder(area, pib, pto_turisticos, populacao));
 
-  printf("\n\nCarta 2:\nEstado: %c\nCódigo: %s\nNome: %s\nPopulação: %d habitantes\nÁrea: %.2f km²\nPIB: %.2f Bilhões de reais\nQuantidade de pontos turísticos: %d\nDensidade populacional: %.2f hab/km²\nPib per capta: %.2f reais\n",
-  estado_2, codigo_2, nome_2, populacao_2, area_2, pib_2, pto_turisticos_2, densidade_pop(populacao_2, area_2), pib_capta(pib_2, populacao_2));
+  printf("\n\nCarta 2:\nEstado: %c\nCódigo: %s\nNome: %s\nPopulação: %d habitantes\nÁrea: %.2f km²\nPIB: %.2f Bilhões de reais\nQuantidade de pontos turísticos: %d\nDensidade populacional: %.2f hab/km²\nPib per capta: %.2f reais\nSuper poder: %.2f\n",
+  estado_2, codigo_2, nome_2, populacao_2, area_2, pib_2, pto_turisticos_2, densidade_pop(populacao_2, area_2), pib_capta(pib_2, populacao_2), super_poder(area_2, pib_2, pto_turisticos_2, populacao_2));
+
+  //Comparacoes:
+
+  printf("\n\nComparações:\n\n");
+
+  //populacao
+  printf("populacao:\n");
+  printf("Empate = %d\n", populacao == populacao_2);
+  printf("Carta 1 Venceu = %d\n\n", populacao > populacao_2);
+
+  //area
+  printf("area\n");
+  printf("Empate = %d\n", area == area_2);
+  printf("Carta 1 Venceu = %d\n\n", area > area_2);
+
+  //pib
+  printf("pib\n");
+  printf("Empate = %d\n", pib == pib_2);
+  printf("Carta 1 Venceu = %d\n\n", pib > pib_2);
+
+  //pontos turisticos
+  printf("pontos turisticos\n");
+  printf("Empate = %d\n", pto_turisticos == pto_turisticos_2);
+  printf("Carta 1 Venceu = %d\n\n", pto_turisticos > pto_turisticos_2);
+
+  //densidade populacional
+  printf("densidade populacional\n");
+  printf("Empate = %d\n", densidade_pop(populacao, area) == densidade_pop(populacao_2, area_2));
+  printf("Carta 1 Venceu = %d\n\n", densidade_pop(populacao, area) < densidade_pop(populacao_2, area_2));
+
+  //pib per capta
+  printf("pib percapta\n");
+  printf("Empate = %d\n", pib_capta(pib, populacao) == pib_capta(pib_2, populacao_2));
+  printf("Carta 1 Venceu = %d\n\n", pib_capta(pib, populacao) > pib_capta(pib_2, populacao_2));
+
+  //super poder
+  printf("super poder\n");
+  printf("Empate = %d\n", super_poder(area, pib, pto_turisticos, populacao) == super_poder(area_2, pib_2, pto_turisticos_2, populacao_2));
+  printf("Carta 1 Venceu = %d\n", super_poder(area, pib, pto_turisticos, populacao) > super_poder(area_2, pib_2, pto_turisticos_2, populacao_2));
 
   return 0;
 }
